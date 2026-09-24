0b45682e1c01814d99e73704e95465df8aca6451
Marcelo <mcestudopro@gmail.com>
2026-09-24 09:02:56 -0300

    modificações e ajustes finais

--- consultas/urls.py
+++ consultas/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.buscar_pessoas, name='buscar_pessoas'),
     path('pessoa/<int:pessoa_id>/', views.detalhe_pessoa, name='detalhe_pessoa'),
+    path('venda/<int:venda_id>/pagar/', views.marcar_venda_paga, name='marcar_venda_paga'),
+    path('pessoa/<int:pessoa_id>/pagar-tudo/', views.marcar_todas_pagas, name='marcar_todas_pagas'),
 ]
--- consultas/views.py
+++ consultas/views.py
@@ -1,5 +1,7 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect, get_object_or_404
 from django.db.models import Sum, F
+from django.utils import timezone
 
 from pessoas.models import Pessoa
 from vendas.models import Venda
@@ -26,4 +28,26 @@ def detalhe_pessoa(request, pessoa_id):
 
     return render(request, 'consultas/resultado_detalhe_pessoa.html',{'pessoa': pessoa, 'vendas': vendas, 'valor_total': valor_total, 'valor_pendente': valor_pendente})
 
+@login_required
+def marcar_venda_paga(request, venda_id):
+    venda = get_object_or_404(Venda, id=venda_id)
 
+    if request.method == 'POST':
+        forma_pagamento = request.POST.get('forma_pagamento')
+        venda.forma_pagamento = forma_pagamento
+        venda.pago = True
+        venda.data_pagamento = timezone.now()
+        venda.save()
+
+    return redirect('detalhe_pessoa', pessoa_id=venda.pessoa.id)
+
+@login_required
+def marcar_todas_pagas(request, pessoa_id):
+    pessoa = get_object_or_404(Pessoa, id=pessoa_id)
+
+    if request.method == 'POST':
+        forma_pagamento = request.POST.get('forma_pagamento')
+        vendas = Venda.objects.filter(pessoa=pessoa, pago=False)
+        vendas.update(pago=True, data_pagamento=timezone.now(), forma_pagamento=forma_pagamento)
+
+    return redirect('detalhe_pessoa', pessoa_id=pessoa.id )
